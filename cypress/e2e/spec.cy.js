9ecf9dab610db1e988f29448e8b8ee59503b3e2e
Balde Gopi <gopi.balde@ideabytes.com>
2023-11-22 12:37:20 +0530

    caw assignment

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,38 +1,44 @@
-// Assuming you have already set up Cypress for your project
 
 // Define a function to load test data from a JSON file
-const loadTestDataFromJSON = () => {
-  cy.fixture('test_data.json').as('testData'); // Load test data and alias it as 'testData'
+const readJsonTestData = () => {
+  // Load test data and alias it as 'testData'
+  cy.fixture('test_data.json').as('testData');
 }
 
 describe('Test Data Loading', () => {
   before(() => {
-      // Load the test data once before running the tests
-      loadTestDataFromJSON();
+    // Load the test data once before running the tests
+    readJsonTestData();
   });
 
   it('Should load and use test data', function () {
-      // Access the test data using the alias 'testData'
-      const testData = this.testData;
+    // Access the test data using the alias 'testData'
+    const testData = this.testData;
+    cy.log(JSON.stringify(testData))
 
-      // Assertion: Ensure test data is loaded
-      expect(testData).to.not.be.null;
+    // Assertion: Ensure test data is loaded
+    expect(testData).to.not.be.null;
 
-      // Assertion: Ensure test data has no duplicates
-      // const uniqueTestData = Array.from(new Set(testData));
-      // expect(testData).to.deep.equal(uniqueTestData);
+    // log all company names without diplicates
+    const testCode = this.testData.testData;
+    const logCompanyNamesWithoutDuplicates = new Set();
 
-      // You can use 'testData' for your testing scenarios.
+    testCode.forEach(item => {
+      logCompanyNamesWithoutDuplicates.add(item.company_name);
+
+    });
+
+    // Convert the Set back to an array if needed
+    const logCompanyNamesWithoutDuplicatesArray = Array.from(logCompanyNamesWithoutDuplicates);
+    // cy.log(logCompanyNamesWithoutDuplicatesArray.length)
+    // Print the unique company names
+    logCompanyNamesWithoutDuplicatesArray.forEach(companyName => {
+      cy.log(companyName);
+      //asserting the value count without duplicates
+      expect(logCompanyNamesWithoutDuplicatesArray.length).to.equal(3);
+    });
+
   });
 
-  it('Should access data by a specific key', function () {
-      // Access the test data using the alias 'testData'
-      const testData = this.testData;
 
-      // // Example: Get data by a specific key (e.g., 'test_case_name')
-      // const dataByKey = testData.filter(item => item.test_case_name);
-
-      // // Assertion: Ensure data is retrieved by the specified key
-      // expect(dataByKey.length).to.be.greaterThan(0);
-  });
 });
